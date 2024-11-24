c760657fa7ea64f7f8d9f59dcdd4942c3858eb65
kacperszczudlo <kacper.szczudlo@gmail.com>
2024-11-24 22:20:41 +0100

    new

--- III semestr/android/lab6/app/src/main/java/com/example/worldtimeapp/TimeActivity.java
+++ III semestr/android/lab6/app/src/main/java/com/example/worldtimeapp/TimeActivity.java
@@ -20,13 +20,11 @@ public class TimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
 
-        // Inicjalizacja TextView dla każdej strefy czasowej
         tvLocalTime = findViewById(R.id.tvLocalTime);
         tvNewYorkTime = findViewById(R.id.tvNewYorkTime);
         tvLondonTime = findViewById(R.id.tvLondonTime);
         tvTokyoTime = findViewById(R.id.tvTokyoTime);
 
-        // Rozpocznij dynamiczne odświeżanie czasu
         startUpdatingTime();
     }
 
@@ -35,36 +33,26 @@ public class TimeActivity extends AppCompatActivity {
             @Override
             public void run() {
                 updateTimes();
-                handler.postDelayed(this, 1000); // Odświeżanie co 1 sekundę
+                handler.postDelayed(this, 1000);
             }
         }, 0);
     }
 
     private void updateTimes() {
-        // Ustawienie formatu czasu (HH:mm:ss)
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss", Locale.getDefault());
-
-        // Lokalny czas (automatycznie obsługuje zmianę czasu)
-        ZonedDateTime localTime = ZonedDateTime.now();
-        tvLocalTime.setText("Mobile time\n" + localTime.format(formatter));
-
-        // Czas w Nowym Jorku (automatycznie obsługuje zmianę czasu)
+        ZonedDateTime warsawTime = ZonedDateTime.now(ZoneId.of("Europe/Warsaw"));
+        tvLocalTime.setText(warsawTime.format(formatter));
         ZonedDateTime newYorkTime = ZonedDateTime.now(ZoneId.of("America/New_York"));
-        tvNewYorkTime.setText("New York\n" + newYorkTime.format(formatter));
-
-        // Czas w Londynie (automatycznie obsługuje zmianę czasu)
+        tvNewYorkTime.setText(newYorkTime.format(formatter));
         ZonedDateTime londonTime = ZonedDateTime.now(ZoneId.of("Europe/London"));
-        tvLondonTime.setText("London\n" + londonTime.format(formatter));
-
-        // Czas w Tokio (zawsze standardowy czas, brak zmiany czasu)
+        tvLondonTime.setText(londonTime.format(formatter));
         ZonedDateTime tokyoTime = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
-        tvTokyoTime.setText("Tokyo\n" + tokyoTime.format(formatter));
+        tvTokyoTime.setText(tokyoTime.format(formatter));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        // Zatrzymanie odświeżania, aby uniknąć wycieków pamięci
         handler.removeCallbacksAndMessages(null);
     }
 }
